0265fae5bd66514e114d52646bb6980590715e71
projetotargetcripto-code <projetotargetcripto@gmail.com>
2025-08-16 21:15:59 -0300

    refactor: dedupe debug connection route

--- src/App.tsx
+++ src/App.tsx
@@ -178,10 +178,7 @@ const App = () => (
             <Route path="/terrenista/status" element={<Protected allowedRoles={['terrenista']}><PanelSectionPage menuKey="terrenista" title="Terrenista" section="Status" /></Protected>} />
             <Route path="/terrenista/pagamentos" element={<Protected allowedRoles={['terrenista']}><PanelSectionPage menuKey="terrenista" title="Terrenista" section="Pagamentos" /></Protected>} />
             
-            {/* Debug routes */}
-            <Route path="/debug/connection" element={<div style={{all:'initial'}}><div id="debug-connection-root"></div></div>} />
-            
-            {/* Load debug component dynamically */}
+            {/* Debug routes - loaded dinamicamente */}
             <Route path="/debug/connection" lazy={async () => {
               const Component = (await import("./app/debug/connection/page")).default;
               return { Component };
